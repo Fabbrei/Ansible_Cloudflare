f9afb68144353f8bb86d5318aa94c6246657dda6
fabbrei <fabrizio.dicesare@studio.unibo.it>
2023-10-17 22:29:38 +0200

    first zone creation function implemented

--- ansible_cloudflare/library/zone.py
+++ ansible_cloudflare/library/zone.py
@@ -74,12 +74,77 @@ import CloudFlare
 
 
 
+    
+def create_zone(cf=None, name=None, account_id=None):
+    result = dict(
+        changed=False,
+        zone_id=''
+    )
+
+    return_status = False
+
+    msg = ""
+
+    if not cf:
+        msg = "Object Cloudflare is None"
+        return return_status, result, msg
+
+    current_zones = None
+
+    try:
+        if account_id:
+            current_zones = cf.zones.get(params={"account": {"id": account_id}})
+        else:
+            current_zones = cf.zones.get()
+
+    except Exception as e:
+        msg = f"Failed to retrieve zones\nError:{e}"
+        return return_status, result, msg
+
+
+    if not current_zones:
+        msg = "Failed to retrieve zones, current_zones is None"
+        return return_status, result, msg
+
+
+    for zone in current_zones:
+        if zone['name'] == name:
+            msg = f"Zone {name} already on Cloudflare account"
+            return_status = True
+            result['zone_id'] = zone['id']
+            return return_status, result, msg
+
+    
+    
+    data = {"name": name}
+
+    if account_id:
+        data.update({"account":{"id": account_id}})
+
+    new_zone = None
+
+    try:
+        
+        new_zone = cf.zones.post(data=data)
+    
+    except Exception as e:
+        msg = f"Failed to create zone {name}\nError:{e}"
+        return return_status, result, msg
+
+    return_status = True
+    result['changed'] = True
+    result['zone_id'] = new_zone['id']
+
+    return return_status, result, msg
+
+
+
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
         email=dict(type='str', required=True),
         api_key=dict(type='str', required=True),
-        account_id=dict(type='str', required=False)
+        account_id=dict(type='str', required=False),
         name=dict(type='str', required=True)
     )
     
@@ -108,11 +173,20 @@ def run_module():
     if module.check_mode:
         module.exit_json(**result)
 
-    if module.params['name'] == 'fail me':
-        module.fail_json(msg='You requested this to fail', **result)
+    cf = None
 
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
+    try:
+        cf = CloudFlare.CloudFlare(email=module.params['email'], key=module.params['api_key'])
+    except Exception as e:
+        msg = f"Failed to initialize Cloudflare API\nError: {e}"
+        module.fail_json(msg=msg, **result)
+    
+
+    return_status, result, msg = create_zone(cf, module.params['name'], module.params['account_id'])
+
+    if not return_status:
+        module.fail_json(msg=msg, **result)
+    
     module.exit_json(**result)
 
 
