d797bcdf356df03406bcfa32bb28d744cdf1a35b
Massimo Schembri <massimo.schembri18@gmail.com>
2023-10-17 21:59:29 +0200

    Add module input/output

--- ansible_cloudflare/library/zone.py
+++ ansible_cloudflare/library/zone.py
@@ -70,15 +70,19 @@ message:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
+import CloudFlare
+
 
 
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        name=dict(type='str', required=True),
-        new=dict(type='bool', required=False, default=False)
+        email=dict(type='str', required=True),
+        api_key=dict(type='str', required=True),
+        account_id=dict(type='str', required=False)
+        name=dict(type='str', required=True)
     )
-
+    
     # seed the result dict in the object
     # we primarily care about changed and state
     # changed is if this module effectively modified the target
@@ -86,8 +90,7 @@ def run_module():
     # for consumption, for example, in a subsequent task
     result = dict(
         changed=False,
-        original_message='',
-        message=''
+        zone_id=''
     )
 
     # the AnsibleModule object will be our abstraction working with Ansible
@@ -105,19 +108,6 @@ def run_module():
     if module.check_mode:
         module.exit_json(**result)
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
-    result['original_message'] = module.params['name']
-    result['message'] = 'goodbye'
-
-    # use whatever logic you need to determine whether or not this module
-    # made any modifications to your target
-    if module.params['new']:
-        result['changed'] = True
-
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
     if module.params['name'] == 'fail me':
         module.fail_json(msg='You requested this to fail', **result)
 
